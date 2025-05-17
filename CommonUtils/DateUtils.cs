40c0801af17ae0c61c02c07602d86a25276f59b9
Just Machiavelli <1780089183@qq.com>
2025-05-17 13:37:22 +0800

    企稳策略

--- CommonUtils/DateUtils.cs
+++ CommonUtils/DateUtils.cs
@@ -1,6 +1,6 @@
 ﻿namespace HappreeTool.CommonUtils
 {
-    internal class DateUtils
+    public class DateUtils
     {
 
         /// <summary>
@@ -13,6 +13,22 @@
         {
             TimeSpan difference = endDate - startDate;
             return (int)difference.TotalDays;
+        }
+
+        public static List<DateOnly> GetNextSundays(DateOnly startDate, int count)
+        {
+            var sundays = new List<DateOnly>();
+
+            // 先找出 startDate 之后的第一个周日（包含当天的话加上判断）
+            int daysUntilSunday = ((int)DayOfWeek.Sunday - (int)startDate.DayOfWeek + 7) % 7;
+            var firstSunday = startDate.AddDays(daysUntilSunday == 0 ? 7 : daysUntilSunday); // 不包含 startDate 当天
+
+            for (int i = 0; i < count; i++)
+            {
+                sundays.Add(firstSunday.AddDays(i * 7));
+            }
+
+            return sundays;
         }
 
     }
